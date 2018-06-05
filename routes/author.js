2973d017edd21200acf7b0fd7b55c13ece3c951e
songxianling <360198419@qq.com>
2018-06-05 18:39:24 +0800

    作者中心

--- routes/author.js
+++ routes/author.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('author',{con:'user-page'});
+router.get('/*', function (req, res, next) {
+  console.log(req.query);
+  
+  res.render('author', {
+    title: '个人中心',
+    con: 'user-page'
+  });
 });
-router.get('/abc', function(req, res, next) {
-    res.send('abc users page');
+router.get('/abc', function (req, res, next) {
+  res.send('abc users page');
 });
 
 module.exports = router;
