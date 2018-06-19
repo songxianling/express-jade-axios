64fe18c73658d3c351c322b68629fc552241a5f1
songxianling <360198419@qq.com>
2018-06-19 18:35:12 +0800

    404组件

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,36 +1,23 @@
-let $openAppBtn = $('.js-open-app');
+$(document).ready(() => {
+    let $openAppBtn = $('.js-open-app');
 
-$.ajax({
-    type: "POST",
-    url: "/desc",
-    data: {
-        'url':'/DishMenus/Main2/Search/searchResult',
-        "keywords": "素菜",
-        "type": "caipu",
-        'page': 2
-    },
-    dataType: "json",
-    success: function (data) {
-        // console.log(data)
+    $openAppBtn.on("click", (event) => {
+        console.log('打开APP');
+        handleFn.openApp();
+        event.stopPropagation();
+    })
+
+    let handleFn = {
+        openApp: () => {
+            let isIos = navigator.userAgent.match(/iPhone|iPad|iPod/i);
+            let url = isIos ? 'xianghalink://' : 'xiangha://welcome?';
+            url += "showWeb.app?url=" + encodeURIComponent(location.href);
+            let h5CallApp = new callApp();
+            h5CallApp.init({
+                appUrl: url,
+                waitTime: 3000,
+                downLoadUrl: 'https://m.xiangha.com/download/android?c=dishTags'
+            });
+        }
     }
-})
-
-$openAppBtn.on("click", (event) => {
-    console.log('打开APP');
-    handleFn.openApp();
-    event.stopPropagation();
-})
-
-let handleFn = {
-    openApp: () => {
-        let isIos = navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        let url = isIos ? 'xianghalink://' : 'xiangha://welcome?';
-        url += "showWeb.app?url=" + encodeURIComponent(location.href);
-        let h5CallApp = new callApp();
-        h5CallApp.init({
-            appUrl: url,
-            waitTime: 3000,
-            downLoadUrl: 'https://m.xiangha.com/download/android?c=dishTags'
-        });
-    }
-}
+});
--- routes/home.js
+++ routes/home.js
@@ -19,87 +19,92 @@ var xsjEnv = process.env.NODE_ENV;
  */
 router.get('/', function (req, res, next) {
     let ApiUrl;
-    console.log(req.query);
-
+    console.log(`参数为${req.query.svCode}`);
     ApiUrl = `${process.env.BASE_URL}:${process.env.BASE_PORT}`;
-    // 获取视频详情
-    var p1 = new Promise((resolve, reject) => {
-        var url = `${ApiUrl}/V1/Content/getInfo?svCode=${req.query.svCode}`;
-        // var url = `https://www.easy-mock.com/mock/5b1773bafbe46869c0b686c8/xsj/V1/Content/getInfo`;
-        console.log(url);
+    if (req.query.svCode) {
+        // 获取视频详情
+        var p1 = new Promise((resolve, reject) => {
+            var url = `${ApiUrl}/V1/Content/getInfo?svCode=${req.query.svCode}`;
+            console.log(url);
 
-        axios.get(url).then(function (data) {
-            resolve(data.data.data)
-            // resolve('one')
-            // console.log(data.data.data);
-        }).catch(function (error) {
-            reject('error1');
-        });
-    })
-    // 获取作者热门视频
-    var p2 = new Promise((resolve, reject) => {
-        var url = `${ApiUrl}/V1/Content/getContent?id=4&limit=6&userCode=${req.query.userCode}`;
-        // var url = `https://www.easy-mock.com/mock/5b1773bafbe46869c0b686c8/xsj/V1/Content/getContent`;
-        console.log(url);
-        axios.get(url).then(function (data) {
-            // 赋值热门视频
-            data.data.data.hotVideoList = data.data.data.list;
-            delete data.data.data.list;
-            resolve(data.data.data)
-            // console.log(data.data.data);
-        }).catch(function (error) {
-            reject('error2');
-        });
-    })
-    // 获取推荐热门视频
-    var p3 = new Promise((resolve, reject) => {
-        var url = `${ApiUrl}/V1/Content/getContent?id=2&userCode=${req.query.userCode}`;
-        // var url = `https://www.easy-mock.com/mock/5b1773bafbe46869c0b686c8/xsj/V1/Content/getContent`;
-        console.log(url);
-        axios.get(url).then(function (data) {
-            data.data.data.recomVideoList = data.data.data.list;
-            delete data.data.data.list;
-            resolve(data.data.data)
-            // resolve('one')
-            // console.log(data.data.data);
-        }).catch(function (error) {
-            reject('error3');
-        });
-    })
+            axios.get(url).then(function (data) {
+                resolve(data.data.data)
+                // resolve('one')
+                // console.log(data.data.data);
+            }).catch(function (error) {
+                reject('error1');
+            });
+        })
+        // 获取作者热门视频
+        var p2 = new Promise((resolve, reject) => {
+            var url = `${ApiUrl}/V1/Content/getContent?id=4&limit=6&userCode=${req.query.userCode}`;
+            console.log(url);
+            axios.get(url).then(function (data) {
+                // 赋值热门视频
+                data.data.data.hotVideoList = data.data.data.list;
+                delete data.data.data.list;
+                resolve(data.data.data)
+                // console.log(data.data.data);
+            }).catch(function (error) {
+                reject('error2');
+            });
+        })
+        // 获取推荐热门视频
+        var p3 = new Promise((resolve, reject) => {
+            var url = `${ApiUrl}/V1/Content/getContent?id=2&userCode=${req.query.userCode}`;
+            console.log(url);
+            axios.get(url).then(function (data) {
+                data.data.data.recomVideoList = data.data.data.list;
+                delete data.data.data.list;
+                resolve(data.data.data)
+                // resolve('one')
+                // console.log(data.data.data);
+            }).catch(function (error) {
+                reject('error3');
+            });
+        })
 
-    Promise.all([p1, p2, p3]).then((result) => {
-        let resDate = {};
-        // 拼装所有返回的数据
-        for (let i = 0; i < result.length; i++) {
-            let cur = result[i];
-            if (cur.state == 2) {
-                for (let i in cur) {
-                    resDate[i] = cur[i];
+        Promise.all([p1, p2, p3]).then((result) => {
+            let resDate = {};
+            // 拼装所有返回的数据
+            for (let i = 0; i < result.length; i++) {
+                let cur = result[i];
+                if (cur.state == 2) {
+                    for (let i in cur) {
+                        resDate[i] = cur[i];
+                    }
                 }
             }
-        }
-        console.log(resDate);
-        res.render('home', {
-            title: resDate.info.desc,
-            modelObj: resDate,
-        });
-        // res.json(result) //['成功了', 'success']
-    }).catch((error) => {
-        console.log('errorAll')
-    })
-
-    var url = `https://www.easy-mock.com/mock/5b1773bafbe46869c0b686c8/xsj/home`;
-    
+            // console.log(resDate);
+            res.render('home', {
+                title: resDate.info.desc,
+                modelObj: resDate,
+            });
+            // res.json(result) //['成功了', 'success']
+        }).catch((error) => {
+            console.log('errorAll')
+        })
+    } else {
+        // 获取推荐热门视频
+        var p3 = new Promise((resolve, reject) => {
+            var url = `${ApiUrl}/V1/Content/getContent?id=2`;
+            console.log(url);
+            axios.get(url).then(function (data) {
+                data.data.data.recomVideoList = data.data.data.list;
+                delete data.data.data.list;
+                resolve(data.data.data)
+                // resolve('one')
+                console.log(data.data.data);
+                res.render('error', {
+                    title: '没找到视频信息～',
+                    modelObj: data.data.data,
+                });
+            }).catch(function (error) {
+                reject('error3');
+            });
+        })
+    }
 });
 
-router.get('/getUser', function (req, res, next) {
-    res.send(user)
-})
-
-router.post('/desc', function (req, res, next) {
-    console.log('前端请求');
-    console.log('ajax=======>' + req.url);
-    console.log(req.body.url);
-})
 
 module.exports = router;
