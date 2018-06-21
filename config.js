69b98ce505f2969086b07a812afda5727a18237b
songxianling <360198419@qq.com>
2018-06-21 09:38:45 +0800

    修改为mock数据

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // 修改API地址文件也需要重新手动启动服务
 
 module.exports = {
-  'DevApi': 'http://apiweb.xhyyym1.com',
-  'DevPort': '8801',
+  'DevApi': 'https://www.easy-mock.com',
+  'DevPort': '/mock/5b1773bafbe46869c0b686c8/xsj',
   'TestApi': 'http://apiweb.xhyyym1.com',
   'TestPort': '7777',
   'ProdApi': 'http://apiweb.xhyyym1.com',
