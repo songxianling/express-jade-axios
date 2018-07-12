8ef4993fd3284810d997cbfa6a096b3b75450f19
songxianling <360198419@qq.com>
2018-07-12 17:55:36 +0800

    404处理js

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var Home = require('./home');
 var Author = require('./author');
 app.use('/', Home);
 app.use('/author', Author);
+// 统一添加404处理
 app.get('*', function (req, res) {
     res.render('404', {
         title: '哇哦～找不到了',
